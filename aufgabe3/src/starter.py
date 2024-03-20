4f90440c8f5725a9290d530b0dbf6ff915ae43be
M1ngX1ao <97147464+m1ngx1ao@users.noreply.github.com>
2024-03-20 16:45:18 +0100

    ex3: src in Unterordner

--- aufgabe3/src/starter.py
+++ aufgabe3/src/starter.py
@@ -6,5 +6,6 @@ beobachter = [Logger(), Stats(), Plotter()]
 #param = Parameter(min_abstand=50,sicher_abstand_ab=100)
 param = Parameter()
 #GierigLauf('siedler3', beobachter, param).tue()
-for i in range(1, 6):
-	GierigLauf(f'siedler{i}', beobachter, param).tue()
+GierigLauf('griechenland', beobachter, param).tue()
+#for i in range(1, 6):
+#	GierigLauf(f'siedler{i}', beobachter, param).tue()
